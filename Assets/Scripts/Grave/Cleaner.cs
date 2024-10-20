a654cc9ee1894650bbfeb27d10b5bb7f71f00dfc
Zoe <a23zoeguepar@inspedralbes.cat>
2024-10-20 16:43:57 +0200

    Limpieza funciona con el raton.

--- Assets/Scripts/Grave/Cleaner.cs
+++ Assets/Scripts/Grave/Cleaner.cs
@@ -6,30 +6,16 @@ using UnityEngine.UI;
 
 public class Cleaner : MonoBehaviour
 {
-    [SerializeField] private Material _material;
-
-    [FormerlySerializedAs("_dirtMaskTexture")] [SerializeField]
-    private Texture2D _dirtMaskTextureBase;
-
     [SerializeField] private Texture2D _dirtBrush;
-
-    private Texture2D _dirtyMaskTexture;
-
-    [SerializeField] private float _dirtAmountTotal = 0f;
-
-    [FormerlySerializedAs("_dirtAmountActual")] [FormerlySerializedAs("_dirtamountActual")] [SerializeField]
-    private float _dirtAmount;
-
-    [SerializeField] private Text _percentageText;
 
     // Start is called before the first frame update
     void Awake()
     {
-        _dirtyMaskTexture = new Texture2D(_dirtMaskTextureBase.width, _dirtMaskTextureBase.height);
-        _dirtyMaskTexture.SetPixels(_dirtMaskTextureBase.GetPixels());
-        _dirtyMaskTexture.Apply();
-        _material.SetTexture("_DirtyMask", _dirtyMaskTexture);
-        CalculatePixel();
+        // _dirtyMaskTexture = new Texture2D(_dirtMaskTextureBase.width, _dirtMaskTextureBase.height);
+        // _dirtyMaskTexture.SetPixels(_dirtMaskTextureBase.GetPixels());
+        // _dirtyMaskTexture.Apply();
+        // _material.SetTexture("_DirtyMask", _dirtyMaskTexture);
+        //CalculatePixel();
     }
 
     // Update is called once per frame
@@ -40,53 +26,41 @@ public class Cleaner : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit))
             {
                 Vector2 textureCoord = raycastHit.textureCoord;
-
-                int PixelX = (int)(textureCoord.x * _dirtMaskTextureBase.width);
-                int PixelY = (int)(textureCoord.y * _dirtMaskTextureBase.height);
-
-                Vector2Int paintPixelPosition = new Vector2Int(PixelX, PixelY);
-                Debug.Log("UV: " + textureCoord + "; Pixels: " + paintPixelPosition);
-
-                int pixelXOffset = PixelX - (_dirtBrush.width / 2);
-                int pixelYOffset = PixelY - (_dirtBrush.height / 2);
-
-                for (int i = 0; i < _dirtBrush.width; i++)
+                if (raycastHit.collider.TryGetComponent<ClenableProp>(out ClenableProp clenableProp))
                 {
-                    for (int j = 0; j < _dirtBrush.height; j++)
+                    int PixelX = (int)(textureCoord.x * clenableProp.dirtMaskTextureBase.width);
+                    int PixelY = (int)(textureCoord.y * clenableProp.dirtMaskTextureBase.height);
+
+                    Vector2Int paintPixelPosition = new Vector2Int(PixelX, PixelY);
+                    //Debug.Log("UV: " + textureCoord + "; Pixels: " + paintPixelPosition);
+
+                    int pixelXOffset = PixelX - (_dirtBrush.width / 2);
+                    int pixelYOffset = PixelY - (_dirtBrush.height / 2);
+
+                    for (int i = 0; i < _dirtBrush.width; i++)
                     {
-                        Color pixelDirt = _dirtBrush.GetPixel(i, j);
-                        Color pixelDirtMask = _dirtyMaskTexture.GetPixel(pixelXOffset + i, pixelYOffset + j);
+                        for (int j = 0; j < _dirtBrush.height; j++)
+                        {
+                            Color pixelDirt = _dirtBrush.GetPixel(i, j);
+                            Color pixelDirtMask =
+                                clenableProp.dirtyMaskTexture.GetPixel(pixelXOffset + i, pixelYOffset + j);
 
-                        float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
-                        _dirtAmount -= removedAmount;
-                        int percentage = Mathf.RoundToInt(_dirtAmount / _dirtAmountTotal * 100);
-                        _percentageText.text = "" + percentage;
+                            float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
+                            clenableProp.dirtAmount -= removedAmount;
+                            int percentage =
+                                Mathf.RoundToInt(clenableProp.dirtAmount / clenableProp.dirtAmountTotal * 100);
+                            clenableProp.percentageText.text = "" + percentage;
 
-                        _dirtyMaskTexture.SetPixel(
-                            pixelXOffset + i,
-                            pixelYOffset + j,
-                            new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                            clenableProp.dirtyMaskTexture.SetPixel(
+                                pixelXOffset + i,
+                                pixelYOffset + j,
+                                new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                        }
                     }
+
+                    clenableProp.dirtyMaskTexture.Apply();
                 }
-
-                _dirtyMaskTexture.Apply();
             }
         }
-
-        //int pixelXOffset = PixelX - 
-    }
-
-    void CalculatePixel()
-    {
-        for (int i = 0; i < _dirtMaskTextureBase.width; i++)
-        {
-            for (int j = 0; j < _dirtMaskTextureBase.height; j++)
-            {
-                _dirtAmountTotal += _dirtMaskTextureBase.GetPixel(i, j).g;
-            }
-        }
-
-        _dirtAmount = _dirtAmountTotal;
-        _percentageText.text = "100";
     }
 }
--- Assets/Scripts/Grave/ClenableProp.cs
+++ Assets/Scripts/Grave/ClenableProp.cs
@@ -1,23 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class ClenableProp : MonoBehaviour
 {
     public Material Material;
 
-    [SerializeField] private Texture2D _dirtMaskTextureBase;
-
+    [FormerlySerializedAs("_dirtMaskTextureBase")] [SerializeField] private Texture2D _dirtyMaskTextureBase;
     private Texture2D _dirtyMaskTexture;
+    public Texture2D dirtyMaskTexture { get => _dirtyMaskTexture; private set => _dirtyMaskTexture = value; }
+    public Texture2D dirtMaskTextureBase
+    {
+        get => _dirtyMaskTextureBase;
+        private set => _dirtyMaskTextureBase = value;
+    }
+    public float dirtAmount
+    {
+        get => _dirtAmount;
+        set => _dirtAmount = value;
+    }
+
+    public float dirtAmountTotal
+    {
+        get => _dirtAmountTotal;
+        set => _dirtAmountTotal = value;
+    }
+
+    public Text percentageText
+    {
+        get => _percentageText;
+        set => _percentageText = value;
+    }
+
+    [SerializeField] private float _dirtAmountTotal = 0f;
+    [SerializeField] private float _dirtAmount;
+    
+    [SerializeField] private Text _percentageText;
 
     // Start is called before the first frame update
     void Awake()
     {
-        Material.SetTexture("_DirtyMask", _dirtyMaskTexture);
+        dirtyMaskTexture = new Texture2D(_dirtyMaskTextureBase.width, _dirtyMaskTextureBase.height);
+        dirtyMaskTexture.SetPixels(_dirtyMaskTextureBase.GetPixels());
+        dirtyMaskTexture.Apply();
+        Material.SetTexture("_DirtyMask", dirtyMaskTexture);
+        CalculatePixel();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CalculatePixel()
     {
+        for (int i = 0; i < dirtMaskTextureBase.width; i++)
+        {
+            for (int j = 0; j < dirtMaskTextureBase.height; j++)
+            {
+                _dirtAmountTotal += dirtMaskTextureBase.GetPixel(i, j).g;
+            }
+        }
+
+        _dirtAmount = _dirtAmountTotal;
+        percentageText.text = "100";
     }
 }
