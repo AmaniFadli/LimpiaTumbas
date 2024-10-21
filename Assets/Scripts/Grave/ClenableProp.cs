57f5e4b8167cbd38ad12f8339e8680205787922c
Zoe <a23zoeguepar@inspedralbes.cat>
2024-10-21 17:01:41 +0200

    CleanAll ready

--- Assets/Scripts/Grave/ClenableProp.cs
+++ Assets/Scripts/Grave/ClenableProp.cs
@@ -21,6 +21,16 @@ public class ClenableProp : MonoBehaviour
     [SerializeField] private Text _percentageText;
     private bool _isClean = false;
 
+    public bool isClean
+    {
+        get => _isClean;
+        set => _isClean = value;
+    }
+
+    [SerializeField] private float _cleanThreshold = 5f;
+
+    private int _cleanPosition;
+
     #endregion
 
     // Start is called before the first frame update
@@ -43,25 +53,32 @@ public class ClenableProp : MonoBehaviour
 
         int pixelXOffset = PixelX - (brush.width / 2);
         int pixelYOffset = PixelY - (brush.height / 2);
-
-        for (int i = 0; i < brush.width; i++)
+        if (!_isClean)
         {
-            for (int j = 0; j < brush.height; j++)
+            for (int i = 0; i < brush.width; i++)
             {
-                Color pixelDirt = brush.GetPixel(i, j);
-                Color pixelDirtMask =
-                    _dirtyMaskTexture.GetPixel(pixelXOffset + i, pixelYOffset + j);
+                for (int j = 0; j < brush.height; j++)
+                {
+                    Color pixelDirt = brush.GetPixel(i, j);
+                    Color pixelDirtMask =
+                        _dirtyMaskTexture.GetPixel(pixelXOffset + i, pixelYOffset + j);
 
-                float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
-                _dirtAmount -= removedAmount;
-                int percentage =
-                    Mathf.RoundToInt(_dirtAmount / _dirtAmountTotal * 100);
-                _percentageText.text = "" + percentage;
+                    float removedAmount = pixelDirtMask.g - (pixelDirtMask.g * pixelDirt.g);
+                    _dirtAmount -= removedAmount;
+                    int percentage =
+                        Mathf.RoundToInt(_dirtAmount / _dirtAmountTotal * 100);
+                    _percentageText.text = "" + percentage;
 
-                _dirtyMaskTexture.SetPixel(
-                    pixelXOffset + i,
-                    pixelYOffset + j,
-                    new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                    _dirtyMaskTexture.SetPixel(
+                        pixelXOffset + i,
+                        pixelYOffset + j,
+                        new Color(0, pixelDirtMask.g * pixelDirt.g, 0));
+                    if (percentage <= _cleanThreshold && !_isClean)
+                    {
+                        print("Clean all");
+                        CleanModel();
+                    }
+                }
             }
         }
 
@@ -84,6 +101,14 @@ public class ClenableProp : MonoBehaviour
 
     void CleanModel()
     {
+        for (int i = 0; i < _dirtyMaskTexture.width; i++)
+        {
+            for (int j = 0; j < _dirtyMaskTexture.width; j++)
+            {
+                _dirtyMaskTexture.SetPixel(i, j, Color.black);
+            }
+        }
+
         _isClean = true;
     }
 }
