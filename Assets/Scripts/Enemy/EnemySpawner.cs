8b75e52639771b33431bb801a9a3f32848568f09
A22Marti <martirb81@gmail.com>
2024-10-23 20:31:03 +0200

    Enemy Jumpscare + Spawns

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,9 +6,28 @@ using UnityEngine.AI;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject mistakeEnemy;
     [SerializeField] private Transform player;
     [SerializeField] private float spawnRadius; // Radio alrededor del jugador donde se puede spawnear
     [SerializeField] private float minSpawnDistance = 5f;  //Distancia a la que se quiere que el enemigo respecto al player
+    [SerializeField] private float minminSpawnTime = 5f;
+    [SerializeField] private float maxSpawnTime = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies(minminSpawnTime, maxSpawnTime)); // Inicia el coroutine al empezar
+    }
+
+    private IEnumerator SpawnEnemies(float minSpawnTime, float maxSpawnTime)
+    {
+        while (true)
+        {
+            SpawnEnemy();
+
+            float randomDelay = Random.Range(minSpawnTime, maxSpawnTime);
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
 
     public void SpawnEnemy()
     {
@@ -17,17 +36,16 @@ public class EnemySpawner : MonoBehaviour
 
         while (!foundPosition)
         {
-        
             Vector3 randomDirection = Random.insideUnitSphere * spawnRadius;
             randomDirection += player.position;
 
-            if (Vector3.Distance(randomDirection, player.position) >= minSpawnDistance) //mirar si el punto de spawn se encuentra dentro del radio y el spawn distance
+            if (Vector3.Distance(randomDirection, player.position) >= minSpawnDistance) // Comprobar si el punto de spawn se encuentra dentro del radio y la distancia mínima
             {
                 NavMeshHit hit;
                 if (NavMesh.SamplePosition(randomDirection, out hit, spawnRadius, NavMesh.AllAreas))
                 {
                     spawnPosition = hit.position;
-                    foundPosition = true; 
+                    foundPosition = true;
                 }
             }
         }
@@ -35,11 +53,27 @@ public class EnemySpawner : MonoBehaviour
         Instantiate(enemy, spawnPosition, Quaternion.identity);
     }
 
-    /*private void Update()
+    public void SpawnEnemyOnMistake()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        Vector3 spawnPosition = Vector3.zero;
+        bool foundPosition = false;
+
+        while (!foundPosition)
         {
-            SpawnEnemy();
+            Vector3 randomDirection = Random.insideUnitSphere * spawnRadius;
+            randomDirection += player.position;
+
+            if (Vector3.Distance(randomDirection, player.position) >= minSpawnDistance) // Comprobar si el punto de spawn se encuentra dentro del radio y la distancia mínima
+            {
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(randomDirection, out hit, spawnRadius, NavMesh.AllAreas))
+                {
+                    spawnPosition = hit.position;
+                    foundPosition = true;
+                }
+            }
         }
-    }*/
+
+        Instantiate(mistakeEnemy, spawnPosition, Quaternion.identity);
+    }
 }
---
+++ Assets/Scripts/Player/GetJumpscared.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class GetJumpscared : MonoBehaviour
+{
+    private Transform targetPoint; 
+    [SerializeField] private float rotationSpeed = 5f;
+    [SerializeField] private GameObject camera;
+    [SerializeField] private GameObject cinemachineCam;
+    [SerializeField] private UnityEngine.InputSystem.PlayerInput input;
+    [SerializeField] private PCamera cameraBlock;
+    private bool canRotate = true;
+    
+
+    private bool isActivated = false;
+
+    public void ActivateRotation(Transform targetPoint)
+    {
+        isActivated = true;
+        this.targetPoint = targetPoint;
+        BlockPlayerMovement();
+
+    }
+
+    public void DeactivateRotation()
+    {
+        isActivated = false;
+    }
+
+    void Update()
+    {
+        if (canRotate && isActivated && targetPoint != null)
+        {
+            StartCoroutine(RotateTowardsTarget());
+        }
+    }
+
+    private IEnumerator RotateTowardsTarget()
+    {
+        canRotate = false; 
+        float elapsedTime = 0f; 
+
+        Quaternion initialRotation = camera.transform.rotation; 
+        Vector3 directionToTarget = targetPoint.position - transform.position;
+        directionToTarget.y = 0;
+
+        Quaternion targetRotation = Quaternion.LookRotation(directionToTarget); 
+
+
+        float totalRotationTime = Quaternion.Angle(initialRotation, targetRotation) / rotationSpeed;
+
+        while (elapsedTime < totalRotationTime)
+        {
+
+            camera.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / totalRotationTime);
+            cinemachineCam.transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, elapsedTime / totalRotationTime);
+
+            elapsedTime += Time.deltaTime; 
+            yield return null; 
+        }
+
+
+        camera.transform.rotation = targetRotation;
+        cinemachineCam.transform.rotation = targetRotation;
+
+        BlockPlayerCamera();
+    
+    }
+
+    private void BlockPlayerMovement()
+    {
+        input.enabled = false;
+    }
+
+    private void BlockPlayerCamera()
+    {
+        cameraBlock.enabled = false;
+        
+    }
+}  
