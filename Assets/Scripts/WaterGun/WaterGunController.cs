16331598488d407aad820cdfc44fecaa069157e9
RemyGSP <guillem0405@gmail.com>
2024-10-23 22:11:05 +0200

    Added-GunSound

--- Assets/Scripts/WaterGun/WaterGunController.cs
+++ Assets/Scripts/WaterGun/WaterGunController.cs
@@ -13,10 +13,14 @@ public class WaterGunController : MonoBehaviour
     [Header("RayCast")]
     [SerializeField] private float raycastDistance;
     [SerializeField] private Transform spawnRay;
+    private bool isShooting;
+    private FMOD.Studio.EventInstance waterSound;
+
 
     private bool isTaked;
     void Start()
     {
+        LoadWaterSound();
         isTaked = false;
     }
     public void SetIsTaked(bool isTaked)
@@ -34,6 +38,7 @@ public class WaterGunController : MonoBehaviour
             float shootInput = PlayerInput.instance.GetShootInput();
             if (shootInput == 1)
             {
+                waterSound.setPaused(false);
                 water.SetActive(true);
                 if (Physics.Raycast(spawnRay.position, spawnRay.forward, out RaycastHit raycastHit, raycastDistance))
                 {
@@ -47,6 +52,7 @@ public class WaterGunController : MonoBehaviour
             }
             else
             {
+                waterSound.setPaused(true);
                 water.SetActive(false);
             }
         } 
@@ -55,5 +61,20 @@ public class WaterGunController : MonoBehaviour
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawRay(gameObject.transform.position, gameObject.transform.forward * raycastDistance);
+    }
+
+    private IEnumerator RestartSound()
+    {
+        yield return new WaitForSeconds(52);
+        LoadWaterSound();
+        StartCoroutine(RestartSound());
+    }
+    private void LoadWaterSound()
+    {
+        waterSound = FMODUnity.RuntimeManager.CreateInstance("event:/WaterGun");
+        waterSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
+        waterSound.start();
+        waterSound.setPaused(true);
+        StartCoroutine(RestartSound());
     }
 }
